4d8a34440c8834303f16024ef3c0db692ab7d7ca
JOSnow985 <jaden.olvera@students.snow.edu>
2025-10-28 12:00:52 -0600

    Checkpoint 1: Initial Commit and Description Line

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// Jaden Olvera, 10-28-25, Lab 8: Maze
+Console.Clear();
+Console.WriteLine("Welcome to a Maze Game!");
+Console.WriteLine("Navigate a maze with your arrow keys and try to reach the \"*\"!");
